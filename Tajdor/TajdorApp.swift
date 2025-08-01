2aa8b94bd6eb23e751ec78a16d2211489f32dbb4
Noura fadil <110945017+Noura-sun@users.noreply.github.com>
2025-08-01 15:34:51 +0300

    Initial Commit

---
+++ Tajdor/TajdorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TajdorApp.swift
+//  Tajdor
+//
+//  Created by Noura  on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TajdorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
